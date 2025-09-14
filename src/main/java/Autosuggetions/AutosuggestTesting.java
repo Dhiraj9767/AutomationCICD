c70c22ea22a4606ab6f4be8f3c5e384703bb0038
Dhiraj9767 <dhirajbhosale7777@gmail.com>
2025-09-14 11:37:17 +0530

    "Auto-suggestions"

--- src/main/java/Autosuggetions/AutosuggestTesting.java
+++ src/main/java/Autosuggetions/AutosuggestTesting.java
@@ -1,8 +1,12 @@
 package Autosuggetions;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AutosuggestTesting {
 
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
 
     }
 }
